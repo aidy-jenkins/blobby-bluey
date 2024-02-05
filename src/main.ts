1c326357034f84387ac99b385f44688e604426f0
aidy-jenkins <git@aidyj.uk>
2024-02-05 22:09:14 +0000

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { InteractiveBrowserCredential } from "@azure/identity";
+import { BlobServiceClient } from "@azure/storage-blob";
+
+class main {
+
+    constructor() {
+
+    }
+
+    static async upload() {
+
+    }
+
+    static async download() {
+
+    }
+}
